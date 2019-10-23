1aae4f0602a72b28d8eea3ca622046edb65c0f36
jasoncin <quoccbinh@gmail.com>
2019-10-23 15:50:48 +0700

    First upload

---
+++ ResNet.py
@@ -0,0 +1,266 @@
+import time
+from ops import *
+from utils import *
+from data_generator import DataGenerator
+from transformer import spatial_transformer_network as stn
+
+
+class ResNet(object):
+    def __init__(self, sess, args):
+        self.model_name = 'ResNet'
+        self.sess = sess
+        self.dataset_name = args.dataset
+
+        if self.dataset_name == 'other':
+            path_list_train = "./train.lst"
+            path_list_val = "./val.lst"
+            self.c_dim = n_class = 1
+
+            print("Start loading data.")
+            data_provider = DataGenerator(path_list_train, path_list_val, n_class)
+            (self.train_x, self.train_y, self.img_h, self.img_w), (self.test_x, self.test_y, self.img_hh, self.img_ww) = data_provider.get_data()
+
+
+        self.checkpoint_dir = args.checkpoint_dir
+        self.log_dir = args.log_dir
+
+        self.res_n = args.res_n
+
+        self.epoch = args.epoch
+        self.batch_size = args.batch_size
+        self.iteration = len(self.train_x) // self.batch_size
+
+        self.init_lr = args.lr
+
+
+    def network(self, x, is_training=True, reuse=False):
+        with tf.variable_scope("network", reuse=reuse):
+
+            if self.res_n < 50 :
+                residual_block = resblock
+            else :
+                residual_block = bottle_resblock
+
+            residual_list = get_residual_layer(self.res_n)
+
+            ch = 32 # paper is 64
+            x = conv(x, channels=ch, kernel=3, stride=1, scope='conv')
+
+            for i in range(residual_list[0]) :
+                x = residual_block(x, channels=ch, is_training=is_training, downsample=False, scope='resblock0_' + str(i))
+
+            ########################################################################################################
+
+            x = residual_block(x, channels=ch*2, is_training=is_training, downsample=True, scope='resblock1_0')
+
+            for i in range(1, residual_list[1]) :
+                x = residual_block(x, channels=ch*2, is_training=is_training, downsample=False, scope='resblock1_' + str(i))
+
+            ########################################################################################################
+
+            x = residual_block(x, channels=ch*4, is_training=is_training, downsample=True, scope='resblock2_0')
+
+            for i in range(1, residual_list[2]) :
+                x = residual_block(x, channels=ch*4, is_training=is_training, downsample=False, scope='resblock2_' + str(i))
+
+            ########################################################################################################
+
+            x = residual_block(x, channels=ch*8, is_training=is_training, downsample=True, scope='resblock_3_0')
+
+            for i in range(1, residual_list[3]) :
+                x = residual_block(x, channels=ch*8, is_training=is_training, downsample=False, scope='resblock_3_' + str(i))
+
+            ########################################################################################################
+
+            x = batch_norm(x, is_training, scope='batch_norm')
+            x = relu(x)
+
+            B = tf.shape(x)[0]
+            H = tf.shape(x)[1]
+            W = tf.shape(x)[2]
+            C = tf.shape(x)[3]
+
+            # Add spatial transformer
+            with tf.variable_scope('spatial_transformer_0'):
+                # %% Create a fully-connected layer with 6 output nodes
+                n_fc = 9
+                W_fc1 = tf.Variable(tf.zeros([H * W * C, n_fc]), name='W_fc1')
+
+                # %% Zoom into the image
+                initial = np.array([[0.5, 0, 0], [0, 0.5, 0], [0.5, 0, 0]])
+                initial = initial.astype('float32')
+                initial = initial.flatten()
+
+                b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
+                self.stn_theta = tf.matmul(tf.zeros([B, H * W * C]), W_fc1) + b_fc1
+
+                x = stn(x, self.stn_theta)
+            return x
+
+    ##################################################################################
+    # Model
+    ##################################################################################
+
+    def build_model(self):
+        """ Graph Input """
+        self.train_inptus = tf.placeholder(tf.float32, [self.batch_size, self.img_h, self.img_w, self.c_dim], name='train_inputs')
+        self.train_labels = tf.placeholder(tf.float32, [self.batch_size, self.img_h, self.img_w, self.c_dim], name='train_labels')
+
+        self.test_inptus = tf.placeholder(tf.float32, [len(self.test_x), self.img_hh, self.img_ww, self.c_dim], name='test_inputs')
+        self.test_labels = tf.placeholder(tf.float32, [len(self.test_x), self.img_hh, self.img_ww, self.c_dim], name='test_labels')
+
+        self.lr = tf.placeholder(tf.float32, name='learning_rate')
+
+        """ Model """
+        self.train_logits = self.network(self.train_inptus)
+        # self.test_logits = self.network(self.test_inptus, is_training=False, reuse=True)
+
+        self.train_loss = classification_loss(labels=self.train_labels, theta=self.stn_theta, org=self.train_inptus)
+        self.test_loss = classification_loss(labels=self.test_labels, theta=self.stn_theta, org=self.test_inptus)
+        
+        reg_loss = tf.losses.get_regularization_loss()
+        self.train_loss += reg_loss
+        self.test_loss += reg_loss
+
+
+        """ Training """
+        self.optim = tf.train.MomentumOptimizer(self.lr, momentum=0.9).minimize(self.train_loss)
+
+        """" Summary """
+        self.summary_train_loss = tf.summary.scalar("train_loss", self.train_loss)
+        # self.summary_train_accuracy = tf.summary.scalar("train_accuracy", self.train_accuracy)
+
+        self.summary_test_loss = tf.summary.scalar("test_loss", self.test_loss)
+        # self.summary_test_accuracy = tf.summary.scalar("test_accuracy", self.test_accuracy)
+
+        # self.train_summary = tf.summary.merge([self.summary_train_loss, self.summary_train_accuracy])
+        # self.test_summary = tf.summary.merge([self.summary_test_loss, self.summary_test_accuracy])
+
+    ##################################################################################
+    # Train
+    ##################################################################################
+
+    def train(self):
+        # initialize all variables
+        tf.global_variables_initializer().run()
+
+        # saver to save model
+        self.saver = tf.train.Saver()
+
+        # summary writer
+        self.writer = tf.summary.FileWriter(self.log_dir + '/' + self.model_dir, self.sess.graph)
+
+        # restore check-point if it exits
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+        if could_load:
+            epoch_lr = self.init_lr
+            start_epoch = (int)(checkpoint_counter / self.iteration)
+            start_batch_id = checkpoint_counter - start_epoch * self.iteration
+            counter = checkpoint_counter
+
+            if start_epoch >= int(self.epoch * 0.75) :
+                epoch_lr = epoch_lr * 0.01
+            elif start_epoch >= int(self.epoch * 0.5) and start_epoch < int(self.epoch * 0.75) :
+                epoch_lr = epoch_lr * 0.1
+            print(" [*] Load SUCCESS")
+        else:
+            epoch_lr = self.init_lr
+            start_epoch = 0
+            start_batch_id = 0
+            counter = 1
+            print(" [!] Load failed...")
+
+        # loop for epoch
+        start_time = time.time()
+        for epoch in range(start_epoch, self.epoch):
+            if epoch == int(self.epoch * 0.5) or epoch == int(self.epoch * 0.75) :
+                epoch_lr = epoch_lr * 0.1
+
+            # get batch data
+            for idx in range(start_batch_id, self.iteration):
+                batch_x = self.train_x[idx*self.batch_size:(idx+1)*self.batch_size]
+                batch_y = self.train_y[idx*self.batch_size:(idx+1)*self.batch_size]
+
+                train_feed_dict = {
+                    self.train_inptus : batch_x,
+                    self.train_labels : batch_y,
+                    self.lr : epoch_lr
+                }
+
+                test_feed_dict = {
+                    self.test_inptus : self.test_x,
+                    self.test_labels : self.test_y
+                }
+
+
+                # update network
+                _, train_loss= self.sess.run(
+                    [self.optim,  self.train_loss], feed_dict=train_feed_dict)
+                # self.writer.add_summary(summary_str, counter)
+
+                # test
+                test_loss = self.sess.run(
+                    [ self.test_loss], feed_dict=test_feed_dict)
+                # self.writer.add_summary(summary_str, counter)
+
+                # display training status
+                counter += 1
+                print("Epoch: [%2d] [%5d/%5d] time: %4.4f, learning_rate : %.4f, train_loss: %.4f, test_loss: %.4f" \
+                      % (epoch, idx, self.iteration, time.time() - start_time, epoch_lr, train_loss, 0.01))
+
+            # After an epoch, start_batch_id is set to zero
+            # non-zero value is only for the first epoch after loading pre-trained model
+            start_batch_id = 0
+
+            # save model
+            self.save(self.checkpoint_dir, counter)
+
+        # save model for final step
+        self.save(self.checkpoint_dir, counter)
+
+    @property
+    def model_dir(self):
+        return "{}{}_{}_{}_{}".format(self.model_name, self.res_n, self.dataset_name, self.batch_size, self.init_lr)
+
+    def save(self, checkpoint_dir, step):
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, self.model_name+'.model'), global_step=step)
+
+    def load(self, checkpoint_dir):
+        print(" [*] Reading checkpoints...")
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            counter = int(ckpt_name.split('-')[-1])
+            print(" [*] Success to read {}".format(ckpt_name))
+            return True, counter
+        else:
+            print(" [*] Failed to find a checkpoint")
+            return False, 0
+
+    def test(self):
+        tf.global_variables_initializer().run()
+
+        self.saver = tf.train.Saver()
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+
+        if could_load:
+            print(" [*] Load SUCCESS")
+        else:
+            print(" [!] Load failed...")
+
+        test_feed_dict = {
+            self.test_inptus: self.test_x,
+            self.test_labels: self.test_y
+        }
+
+
+        test_accuracy = self.sess.run(self.test_accuracy, feed_dict=test_feed_dict)
+        print("test_accuracy: {}".format(test_accuracy))
---
+++ data_generator.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import
+from __future__ import division
+
+import numpy as np
+from random import shuffle 
+
+from util.path_util import read_image_list
+import cv2
+
+
+class DataGenerator(object):
+    """
+
+    """
+    def __init__(self, path_list_train, path_list_eval, n_classes):
+        self.n_classes = 1
+        if path_list_train != None:
+            self.list_training = read_image_list(path_list_train, prefix=None)
+            self.size_training = len(self.list_training)
+
+        if path_list_eval != None:
+            self.list_validation = read_image_list(path_list_eval, prefix=None)
+            self.size_validation = len(self.list_validation)
+
+    def get_data(self):
+        return (self._get_list(self.list_training), self._get_list(self.list_validation))
+
+    def _get_list(self, input_list):
+
+        shuffle(input_list)
+        imgs = []
+        new_imgs = []
+
+        masks = []
+        new_masks = []
+
+        max_height = 0
+        max_width = 0
+
+        for index in range(len(input_list)):
+            # if index % 100 == 0:
+            #     print("{}".format(index))
+
+            image_path = input_list[index]
+            mask_path = image_path.replace("images","labels")
+            input_img = cv2.imread(image_path, 0)
+            mask_img = cv2.imread(mask_path, 0)
+
+            imgs.append(input_img)
+            masks.append(mask_img)
+
+            max_width = max(input_img.shape[1], max_width)
+            max_width = max(mask_img.shape[1], max_width)
+            max_height = max(input_img.shape[0], max_height)
+            max_height = max(mask_img.shape[0], max_height)
+
+        for img in imgs:
+            height = img.shape[0]
+            pad_h = max_height - height
+
+            width = img.shape[1]
+            pad_w = max_width - width
+
+            if pad_h + pad_w > 0:
+                npad = ((0, pad_h), (0, pad_w))
+                img = np.pad(img, npad, mode='constant', constant_values=255)
+            new_imgs.append(np.expand_dims(img, 2))
+
+        for mask in masks:
+            height = mask.shape[0]
+            pad_h = max_height - height
+
+            width = mask.shape[1]
+            pad_w = max_width - width
+
+            if pad_h + pad_w > 0:
+                npad = ((0, pad_h), (0, pad_w))
+                mask = np.pad(mask, npad, mode='constant', constant_values=255)
+            new_masks.append(np.expand_dims(mask, 2))
+
+
+        return new_imgs, new_masks, max_height, max_width
